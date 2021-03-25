3b121e5e8e54a9d7ea4133a082c08e8c2132be53
Your Name <you@example.com>
2021-03-25 11:05:04 +0800

    Initial Commit

---
+++ IRMultiplePlayers/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  IRMultiplePlayers
+//
+//  Created by irons on 2021/3/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
